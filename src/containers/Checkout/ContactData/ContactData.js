08da1ad1bc3db891825bbf98b57299ce8ae0afe4
Jussi-Savoranta <jussi.savoranta@gmail.com>
2021-04-09 13:16:00 +0300

    added functionality to send user inputs from delivery form to server and form validity

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,15 +4,92 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderFrom: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text', // these are default html type names so you just need to 
+                    placeholder: 'Your Name' // pass them down as props to input elements
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Zip code'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false,
+                touched: false
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
+            },
+            delivery: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [ 
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: 'fastest',
+                validation: {},
+                valid: true
+            }
         },
+        formIsValid: false,
         loading: false
     }
 
@@ -20,21 +97,17 @@ class ContactData extends Component {
         event.preventDefault(); // preventing the request and to reload the page
         // alert('You continue!');
         this.setState({ loading: true });
+        const formData = {};
+        // setting key: value -pairs to formData[], like email: [user_input], delivery: fastest/cheapest.. and so on
+        for (let formElementIdentifier in this.state.orderFrom) {
+            formData[formElementIdentifier] = this.state.orderFrom[formElementIdentifier].value;
+        };
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price, // total price is only calculated and stored in BurgerBuilder, we neede to get it from there and pass it to checkout and then here
             // just a reminder that in actual shop you should calculate the price on server
             // so that user can't manipulate the price that gets sent to server
-            customer: {
-                name: 'Jii',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '00100',
-                    country: 'Finland'
-                },
-                email: 'test@test.com'
-            },
-            delivery: 'fastest'
+            orderData: formData // user inputs for delivery
         }
         axios.post('/orders.json', order) // base url is created in axios-orders.js -file
             .then(response => {
@@ -46,24 +119,76 @@ class ContactData extends Component {
             });
     }
 
-    render () {
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if(rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if(rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if(rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        return isValid;
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderFrom
+        };
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+        updatedFormElement.touched = true;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+        let formIsValid = true;
+        for (let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+        this.setState({orderFrom: updatedOrderForm, formIsValid: formIsValid});
+    }
+
+    render() {
+        const formElementsArray = [];
+        for (let key in this.state.orderFrom) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderFrom[key]
+            });
+        }
+
         let form = (
-            <form>
-                    <input className={classes.Input} type="text" name="name" placeholder="Your Name"></input>
-                    <input className={classes.Input} type="email" name="email" placeholder="Your Email"></input>
-                    <input className={classes.Input} type="text" name="street" placeholder="Street"></input>
-                    <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"></input>
-                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-                </form>
+            <form onSubmit={this.orderHandler}>
+                {formElementsArray.map(formElement => (
+                    <Input 
+                    key={formElement.id}
+                    elementType={formElement.config.elementType}
+                    elementConfig={formElement.config.elementConfig}
+                    value={formElement.config.value}
+                    shouldValidate={formElement.config.validation}
+                    invalid={!formElement.config.valid}
+                    touched={formElement.config.touched}
+                    changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+                ))}
+                <Button btnType="Success" disabled={!this.state.formIsValid} >ORDER</Button>
+            </form>
         );
         if (this.state.loading) {
             form = <Spinner />
         }
-        return(
+        return (
             <div className={classes.ContactData}>
                 <h4>Enter your Contact Information</h4>
                 {form}
-            </div> 
+            </div>
         );
     }
 }
